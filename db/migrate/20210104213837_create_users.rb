c174a8f914fad156425716003398326cb833c325
Andres-R-Hernandez <andres.hernandez@utexas.edu>
2021-01-04 17:34:52 -0600

    Added Bcrypt

--- db/migrate/20210104213837_create_users.rb
+++ db/migrate/20210104213837_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :name
       t.integer :age
-      t.string :driver_id
+      t.integer :driver_id
       t.string :address
       t.string :email
       t.string :password_digest
