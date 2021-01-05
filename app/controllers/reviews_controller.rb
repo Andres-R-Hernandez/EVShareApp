395ffb8d7dd0a4123ab492adc9df76e1cb998c53
Your Username <you@example.com>
2021-01-05 15:15:26 -0500

    Reviews controller actions and views

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,30 @@
 class ReviewsController < ApplicationController
+
+    def index
+        @reviews = Review.all 
+    end 
+
+    def show
+        @review = Review.find(params[:id])
+    end 
+
+    def new 
+        @review = Review.new 
+    end 
+
+    def create 
+        @review = Review.new(review_params)
+        if @review.valid?
+            @review.save 
+            redirect_to @review
+        else 
+            render :new 
+        end 
+    end 
+
+    private
+     
+    def review_params
+        params.require(:review).permit(:booking_id, :user_id, :description, :star_rating)
+    end 
 end
