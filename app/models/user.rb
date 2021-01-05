2ede3747ac14e0c429e38e14841683c63598be55
Andres-R-Hernandez <andres.hernandez@utexas.edu>
2021-01-05 10:42:55 -0600

    Added index and show for Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :reviews
   has_many :owned_cars
-  has_many :rentals, through: :bookings, class_name: "OwnedCar"
+  has_many :rentals, through: :bookings, source: :owned_car
   has_many :car_models, through: :wish_list_cars
 end
