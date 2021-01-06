232ffdcdae4dbfb03a862ec6336b51b9ae526aed
Andres-R-Hernandez <andres.hernandez@utexas.edu>
2021-01-06 09:45:12 -0600

    completed booking form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,31 +1,31 @@
 class BookingsController < ApplicationController
 
     def index
-        @bookings = Booking.all 
-    end 
+        @bookings = Booking.all
+    end
 
     def show
         @booking = Booking.find(params[:id])
-    end 
+    end
 
-    def new 
-        @booking = Booking.new 
-    end 
+    def new
+        @booking = Booking.new
+    end
 
-    def create 
+    def create
         @booking = Booking.new(booking_params)
         if @booking.valid?
-            @booking.save 
+            @booking.save
             redirect_to @booking
-        else 
-            render :new 
-        end 
-    end 
+        else
+            render :new
+        end
+    end
 
     private
-     
+
     def booking_params
-        params.require(:booking).permit(:user_id, :owned_car, :booking_time)
-    end 
+        params.require(:booking).permit(:user_id, :owned_car_id, :booking_time)
+    end
 
 end
