671688ae4a4493f805b5520024f38ebeae57e520
Your Username <you@example.com>
2021-01-06 16:18:07 -0500

    Reviews Views

--- app/controllers/owned_cars_controller.rb
+++ app/controllers/owned_cars_controller.rb
@@ -1,2 +1,30 @@
 class OwnedCarsController < ApplicationController
+
+    def index
+        @owned_cars = OwnedCar.all
+    end
+    
+    def show
+        @owned_car = OwnedCar.find(params[:id])
+    end
+    
+    def new
+        @owned_car = OwnedCar.new
+    end
+    
+    def create
+        @owned_car = OwnedCar.new(owned_car_params)
+        if @owned_car.valid?
+            @owned_car.save
+            redirect_to @owned_car
+        else
+            render :new
+        end
+    end
+    
+    private
+    
+    def owned_car_params
+        params.require(:owned_car).permit(:year, :price_per_day, :city, :car_model_id, :user_id)
+    end
 end
